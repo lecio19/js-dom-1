e5815468b74fa9b6981450be35756bd7c30f6c4b
lecio19 <alex.macri01@gmail.com>
2024-10-22 16:50:32 +0200

    correction

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ myButton.addEventListener('click', function () {
     if(myDiv.classList.contains("off")){
         myDiv.classList.add('on');
         myDiv.classList.remove('off');
-        myDiv.style.backgroundImage = "url('../img/yellow_lamp.png')"
+        myDiv.style.backgroundImage = "url('../yellow_lamp.png')"
         myButton.innerText="Spegni";
     }
     else {
         myDiv.classList.add('off');
         myDiv.classList.remove('on');
-        myDiv.style.backgroundImage = "url('../img/white_lamp.png')"
+        myDiv.style.backgroundImage = "url('../white_lamp.png')"
         myButton.innerText="Accendi";
     }
 });
